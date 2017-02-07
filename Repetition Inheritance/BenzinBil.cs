9787b8b866601867ea827431bff4a6a0e2cce58c
Daniel Pilanat Østergaard <dani5905@edu.easj.dk>
2017-02-07 11:36:03 +0100

    Oprettede BenzinBil Class, som arver fra Bil-Class. rettede HalvårligEjerAfgit så den passer med Udregning. Oprettede Liste over BenzinBiler. Tilføjede fem objekter. Udskirver properties inden for de 5 Objekter

---
+++ Repetition Inheritance/BenzinBil.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repetition_Inheritance
+{
+    sealed class BenzinBil : Bil
+    {
+        public BenzinBil(string Mærke, double PrisExAfgift, int KøbsÅr, int KmPrLiter, string RegistreringsNr) : base(Mærke, PrisExAfgift, KøbsÅr, KmPrLiter, RegistreringsNr)
+        {
+        }
+
+        public override int HalvÅrligEjerAfgift()
+        {
+            int ejerafgift = 0;
+            if (KmPrLiter < 20)
+            {
+                ejerafgift = 1200;
+            }
+            else if (KmPrLiter >= 20 || KmPrLiter <= 28)
+            {
+                ejerafgift = 600;
+            }
+            if (KmPrLiter > 28)
+            {
+                ejerafgift = 320;
+            }
+
+            return ejerafgift;
+        }
+
+
+
+    }
+}
--- Repetition Inheritance/Program.cs
+++ Repetition Inheritance/Program.cs
@@ -10,16 +10,30 @@ namespace Repetition_Inheritance
     {
         static void Main(string[] args)
         {
-            List<DieselBil> BilListe = new List<DieselBil>();
-            BilListe.Add(new DieselBil("Ford", 5000, 2004, 23, "852456"));
-            BilListe.Add(new DieselBil("BMW", 3400, 2016, 20, "852789", false));
-            BilListe.Add(new DieselBil("BMW", 4000, 2017, 13, "147369"));
-            BilListe.Add(new DieselBil("BMW", 4500, 2009, 30, "321654"));
-            BilListe.Add(new DieselBil("BMW", 3750, 2010, 28, "753421", false));
+            List<DieselBil> DieselListe = new List<DieselBil>();
+            DieselListe.Add(new DieselBil("Ford", 5000, 2004, 23, "852456"));
+            DieselListe.Add(new DieselBil("BMW", 3400, 2016, 20, "852789", false));
+            DieselListe.Add(new DieselBil("BMW", 4000, 2017, 13, "147369"));
+            DieselListe.Add(new DieselBil("BMW", 4500, 2009, 30, "321654"));
+            DieselListe.Add(new DieselBil("BMW", 3750, 2010, 28, "753421", false));
             
-            foreach (DieselBil db in BilListe)
+            foreach (DieselBil db in DieselListe)
             {
                 Console.WriteLine("RegistreringsNr: " + db.RegistreringsNr + "      Den halvårlige ejerafgift er: " + db.HalvÅrligEjerAfgift());
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            
+            List<BenzinBil> BenzinListe = new List<BenzinBil>();
+            BenzinListe.Add(new BenzinBil("Bugatti  ", 7000, 2010, 30, "951847"));
+            BenzinListe.Add(new BenzinBil("DatCar   ", 5000, 2004, 19, "781354"));
+            BenzinListe.Add(new BenzinBil("DankMemes", 7500, 2016, 35, "678345"));
+            BenzinListe.Add(new BenzinBil("Ford     ", 7200, 2008, 36, "847263"));
+            BenzinListe.Add(new BenzinBil("Bugatti  ", 8000, 2017, 28, "327854"));
+
+            foreach (BenzinBil bb in BenzinListe)
+            {
+                Console.WriteLine("Mærke: " + bb.Mærke + "    Halvårlige ejerafgift: " + bb.HalvÅrligEjerAfgift() + "    Købs År " + bb.KøbsÅr + "    Km Pr. L: " + bb.KmPrLiter + "    Reg. Nr: " + bb.RegistreringsNr );
             }
             Console.ReadLine();
         }
