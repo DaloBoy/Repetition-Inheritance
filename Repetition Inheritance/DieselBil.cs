48281da31aa066b55985a6c91518d8ecd40fc233
Daniel Pilanat Østergaard <dani5905@edu.easj.dk>
2017-02-07 10:54:04 +0100

    Ændrede HalvårligEjerAfgift metode til at beregne prisen rigtigt

--- Repetition Inheritance/DieselBil.cs
+++ Repetition Inheritance/DieselBil.cs
@@ -23,7 +23,21 @@ namespace Repetition_Inheritance
 
         public override int HalvÅrligEjerAfgift()
         {
-            int ejerafgift = 1000;
+            int ejerafgift = 0;
+            if (KmPrLiter < 15)
+            {
+                ejerafgift = 2000;
+            }
+            if (KmPrLiter >= 15 || KmPrLiter <= 25)
+            {
+                ejerafgift = 1000;
+            }
+            if (KmPrLiter > 25)
+            {
+                ejerafgift = 350;
+            }
+
+
             if (PartikelFilter == false)
             {
                 ejerafgift += 500;
--- Repetition Inheritance/Program.cs
+++ Repetition Inheritance/Program.cs
@@ -11,11 +11,11 @@ namespace Repetition_Inheritance
         static void Main(string[] args)
         {
             List<DieselBil> BilListe = new List<DieselBil>();
-            BilListe.Add(new DieselBil("Ford", 5000, 2004, 34, "852456"));
-            BilListe.Add(new DieselBil("BMW", 3400, 2016, 34, "852789", false));
-            BilListe.Add(new DieselBil("BMW", 4000, 2017, 34, "147369"));
-            BilListe.Add(new DieselBil("BMW", 4500, 2009, 34, "321654"));
-            BilListe.Add(new DieselBil("BMW", 3750, 2010, 34, "753421", false));
+            BilListe.Add(new DieselBil("Ford", 5000, 2004, 23, "852456"));
+            BilListe.Add(new DieselBil("BMW", 3400, 2016, 20, "852789", false));
+            BilListe.Add(new DieselBil("BMW", 4000, 2017, 13, "147369"));
+            BilListe.Add(new DieselBil("BMW", 4500, 2009, 30, "321654"));
+            BilListe.Add(new DieselBil("BMW", 3750, 2010, 28, "753421", false));
             
             foreach (DieselBil db in BilListe)
             {
